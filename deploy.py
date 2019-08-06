e688791e80321a0a224337ae5c1aa9377b803c81
Liam Stow <liam.stow@outlook.com>
2019-08-06 17:59:46 +1000

    Configured deploy task

---
+++ deploy.py
@@ -0,0 +1,12 @@
+from shutil import copy
+from distutils.dir_util import copy_tree
+
+destination = 'C:\inetpub\wwwroot'
+files = ['index.html', 'sketch.js']
+dirs = ['resources', 'lib']
+
+for source in files:
+    copy(source, destination + '\\' + source)
+
+for source in dirs:
+    copy_tree(source, destination + '\\' + source)
--- sketch.js
+++ sketch.js
@@ -1,5 +1,14 @@
+var mapImage;
+
+function preload() {
+  mapImage = loadImage('resources/AirBoat2.png');
+}
+
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(100,100,100);
+  image(mapImage, 50, 100);
+  imageMode(CENTER);
 }
 
 function draw() {
