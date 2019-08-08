4f1ed81482705ccd26b5df3f1ae27f73669edbaf
Liam Stow <liam.stow@outlook.com>
2019-08-08 13:51:09 +1000

    Now only updating selected tokens

--- sketch.js
+++ sketch.js
@@ -1,19 +1,34 @@
-var mapImage;
-let gazer;
+let mapImage;
+let tokens = [];
+let selected = []; 
 
 function preload() {
   mapImage = loadImage('resources/AirBoat2.png');
-  gazer = new Token('resources/gazer.png');
+  tokens.push(new Token('resources/gazer.png'));
+  tokens.push(new Token('resources/gazer.png'));
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
+  selectToken(tokens[0]);
+}
+
+function mouseClicked() {
+  selectToken(tokens[1]);
 }
 
 function draw() {
   background(100, 100, 100);
   image(mapImage, 200, 200);
-  gazer.update();
-  gazer.render();
+  selected.forEach(token => {
+    token.update();
+  });
+  tokens.forEach(token => {
+    token.render();
+  })
+}
+
+function selectToken(token) {
+  selected = [token];
 }
